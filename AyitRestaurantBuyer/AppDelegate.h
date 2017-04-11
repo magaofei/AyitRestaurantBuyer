f9b9a1d7388d73e385f8fbafe97ac0101ff7a0fb
Mark <mamian521@gmail.com>
2017-04-11 17:38:28 +0800

    Initial Commit

---
+++ AyitRestaurantBuyer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AyitRestaurantBuyer
+//
+//  Created by MAMIAN on 2017/4/11.
+//  Copyright © 2017年 Gaofei Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
